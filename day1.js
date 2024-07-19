3623ee5c7b6be2d44c6e5a2998dd3f091aa6ba8a
PiyushRaj-7699 <piyushraj7308305@gmail.com>
2024-07-19 21:15:02 +0530

    added day 1 work

--- day1.js
+++ day1.js
@@ -1,2 +1,36 @@
-let a = 5
-console.log(a);
+//1
+var num1=12
+console.log(num1)
+
+//2
+let str="Hello World"
+console.log(str)
+
+//3
+const T=true
+console.log(T)
+
+//4
+let num = 23;
+let str2 = "Good";
+let boolean = false;
+let object = { name: "Raj", age: 18 };
+let array = [1, 2, 3, 4, 5];
+
+console.log(typeof number); 
+console.log(typeof string); 
+console.log(typeof boolean); 
+console.log(typeof object); 
+console.log(typeof array); 
+
+//5
+let val = 1;
+console.log(val); 
+val = 2;
+console.log(val); 
+
+//6
+const val2 = 3;
+console.log(val2);
+val2 = 4;
+console.log(val2); 
